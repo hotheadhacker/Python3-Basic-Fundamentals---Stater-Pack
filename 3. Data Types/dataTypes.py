73fea267347c95ce6033a2d52377fef2520936f4
hotheadhacker <isalmanqureshi@gmail.com>
2020-04-20 23:46:08 +0530

    Added Data Types

---
+++ 3. Data Types/dataTypes.py
@@ -0,0 +1,164 @@
+# Compiled by Salman Qureshi
+# github: hotheadhacker
+# web: salmanually.com
+# Socials: salmanually
+
+# Data Types 
+# Getting the Data Type
+# You can get the data type of any object by using the type() function:
+# Print the data type of the variable x:
+x = 5
+print(type(x))
+
+# Setting the Data Type
+# In Python, the data type is set when you assign a value to a variable:
+
+# str
+x = "Hello World"
+print(x)
+print(type(x))
+
+# int
+x = 20
+print(x)
+print(type(x))
+
+# float
+x = 2.0
+print(x)
+print(type(x))
+
+# complex
+x = 1j
+print(x)
+print(type(x))
+
+# list
+x = ["Kashmir", "Needs", "Justice"]
+print(x)
+print(type(x))
+
+# tuple
+x = ("Kashmir", "is", "Heaven", "On", "Earth")
+print(x)
+print(type(x))
+
+# range
+x = range(7)
+print(x)
+print(type(x))
+
+# dict
+x = {"country" : "Kashmir", "captal" : "Srinagar", "states" : 7}
+print(x)
+print(type(x))
+
+# set
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x))
+
+# frozenset
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x))
+
+# bool
+x = True
+print(x)
+print(type(x))
+
+# bytes
+x = b"Hello"
+print(x)
+print(type(x))
+
+# bytesarray
+x = bytearray(5)
+print(x)
+print(type(x))
+
+# memoryview
+x = memoryview(bytes(5))
+print(x)
+print(type(x))
+
+#End
+
+
+print("___________________________Setting the Specific Data Type_____________________________________")
+
+# Setting the Specific Data Type
+# If you want to specify the data type, you can use the following constructor functions:
+
+
+# str
+x = str("Hello World")
+print(x)
+print(type(x))
+
+# int
+x = int(20)
+print(x)
+print(type(x))
+
+# float
+x = float(2.0)
+print(x)
+print(type(x))
+
+# complex
+x = complex(1j)
+print(x)
+print(type(x))
+
+# list
+x = list(("Kashmir", "Needs", "Justice"))
+print(x)
+print(type(x))
+
+# tuple
+x = tuple(("Kashmir", "is", "Heaven", "On", "Earth"))
+print(x)
+print(type(x))
+
+# range
+x = range(7)
+print(x)
+print(type(x))
+
+# dict
+x = dict (country = "Kashmir", captal = "Srinagar", states = 7)
+print(x)
+print(type(x))
+
+# set
+x = (("apple", "banana", "cherry"))
+print(x)
+print(type(x))
+
+# frozenset
+x = frozenset(("apple", "banana", "cherry"))
+print(x)
+print(type(x))
+
+# bool
+x = bool(0)
+print(x)
+print(type(x))
+
+# bytes
+x = bytes(5)
+print(x)
+print(type(x))
+
+# bytesarray
+x = bytearray(5)
+print(x)
+print(type(x))
+
+# memoryview
+x = memoryview(bytes(5))
+print(x)
+print(type(x))
+
